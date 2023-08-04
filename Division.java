9f5e0dc74c1f0c24d799a69867a454d2b0a05a6d
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-08-04 20:57:50 +0530

    import java.util.Scanner; public class Sample {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int num1,num2,num3;         num1=sc.nextInt();         num2=sc.nextInt();         num3=num1/num2;         System.out.printf("%d",num3);     } }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1,num2,num3;
+        num1=sc.nextInt();
+        num2=sc.nextInt();
+        num3=num1/num2;
+        System.out.printf("%d",num3);
+    }
+}
