9630b60a0065d790bc366b4dfef0c7226327c0a3
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-08-16 14:48:08 +0530

    import java.util.Scanner; public class Reverse {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n;         n=sc.nextInt();         int r,sum=0;         while(n!=0)         {             r=n%10;             sum=sum*10+r;             n=n/10;         }         System.out.printf("%d",sum);     } }

---
+++ Reverse_Integer.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Reverse
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int r,sum=0;
+        while(n!=0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        System.out.printf("%d",sum);
+    }
+}
