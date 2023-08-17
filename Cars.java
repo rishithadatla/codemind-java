d6fcfd1afd4dd58a1e224d758716b35e2183284b
Datla vijaya durga devi <119730516+rishithadatla@users.noreply.github.com>
2023-08-17 11:12:57 +0530

    import java.util.Scanner; public class Cars {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n;         n=sc.nextInt();         if(n%4==0)         {             System.out.println(n/4);         }         else         {             System.out.println(n/4+1);         }     } }

---
+++ Cars.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Cars
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n%4==0)
+        {
+            System.out.println(n/4);
+        }
+        else
+        {
+            System.out.println(n/4+1);
+        }
+    }
+}
